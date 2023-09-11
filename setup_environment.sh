df79011a2ab9d8a45b312252069f6b5a2514960c
MinjuKim0217 <asdfghjkl0217@naver.com>
2023-09-11 21:50:34 +0900

    [ADD] Add environment setup script

--- setup_environment.sh
+++ setup_environment.sh
@@ -8,7 +8,8 @@ conda create --name myenv python=3.10.12 -y
 conda activate myenv
 
 # requirements.txt가 있는 디렉토리로 이동
-cd /Users/mjkiim/Desktop/miraeasset-bigfesta-2023
+# cd /Users/mjkiim/Desktop/miraeasset-bigfesta-2023
+cd {여기는_사용자_경로로}
 
 # requirements.txt에 명시된 패키지 설치
 pip install -r requirements.txt
