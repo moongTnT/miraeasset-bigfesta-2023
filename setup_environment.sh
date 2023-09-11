77c0078f450b26e4ebe0bf5947797c272bc278ac
MinjuKim0217 <asdfghjkl0217@naver.com>
2023-09-11 21:46:51 +0900

    [ADD] Add requirements and environment setup script

---
+++ setup_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Anaconda 초기화 (Anaconda 설치 경로에 따라 변경 가능)
+source ~/anaconda3/etc/profile.d/conda.sh
+
+# 가상 환경 생성 및 활성화 (Python 버전 3.10.12로 설정)
+conda create --name myenv python=3.10.12 -y
+conda activate myenv
+
+# requirements.txt가 있는 디렉토리로 이동
+cd /Users/mjkiim/Desktop/miraeasset-bigfesta-2023
+
+# requirements.txt에 명시된 패키지 설치
+pip install -r requirements.txt
